ece6a48564a04cb7a0c97d1479652b6ca031bf65
P1811 <p1811@student.jamk.fi>
2020-06-02 16:21:49 +0300

    added market; "Stuhr"

--- Marktkauf.py
+++ Marktkauf.py
@@ -5,13 +5,13 @@ from datetime import date #imports the date class from datetime lbrary
 market_type = "Marktkauf" #var to set the market type
 
 market_name = ["Loehne", "Herford", "Buende", "Espelkamp", "Belm", "Rinteln", "Bad_Salzuflen" ,"Hameln", "Osnabrueck", "Luebbecke", "Meppen",
-"Wilhelmshaven", "Osterholz_Scharmbeck", "Wunstorf", "Hannover"] # array of market names
+"Wilhelmshaven", "Osterholz_Scharmbeck", "Wunstorf", "Hannover","Stuhr"] # array of market names
 
 weekNumber = date.today().isocalendar()[1] #var which stores the ISO week number
 
 url_parts = ["20018_Loehne", "20004_Herford", "20009_Buende", "20071_Espelkamp", "20015_Belm", "20014_Rinteln", "20013_Bad_Salzuflen", 
 "20046_Hameln", "20002_Osnabrueck", "20007_Luebbecke", "20017_Meppen", "20026_Wilhelmshaven", "20102_Osterholz_Scharmbeck", "20022_Wunstorf", 
-"20025_Hannover"]
+"20025_Hannover","20555_Stuhr"]
 
 for part in url_parts: #for loop to loop through the location keys
  url = "https://static.edeka.de/media/handzettel/MINDEN/MARKTKAUF_"+part+"/blaetterkatalog/pdf/complete.pdf" #API URL
