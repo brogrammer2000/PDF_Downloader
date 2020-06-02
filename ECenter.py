d6592d74a4e6e97d176f2f5c70a414831e2b5392
P1811 <p1811@student.jamk.fi>
2020-06-02 12:09:01 +0300

    Added comments and shebang

--- ECenter.py
+++ ECenter.py
@@ -1,18 +1,19 @@
-import requests
-from datetime import date
+#!/usr/bin/env python3
+import requests #imports the requests library
+from datetime import date #imports the date class from datetime lbrary
 
-market_type = "ECenter"
+market_type = "ECenter" #var to set the market type
 
-market_name = ["Minden", "Porta-Westfalica", "Osnabruck", "Melle", "Bad Nenndorf", "Herford"]
+market_name = ["Minden", "Porta-Westfalica", "Osnabruck", "Melle", "Bad Nenndorf", "Herford"] # array of market names
 
-weekNumber = date.today().isocalendar()[1]
+weekNumber = date.today().isocalendar()[1] #var which stores the ISO week number
 
-city_codes = ["3315", "3317", "3309", "3323", "3325", "3328"]
+city_codes = ["3315", "3317", "3309", "3323", "3325", "3328"] #array of respective location keys from accuweather
 
-for city in city_codes:
- url = "https://static.edeka.de/media/handzettel/MINDEN/"+city+"/blaetterkatalog/pdf/complete.pdf"
- myfile = requests.get(url)
- for name in market_name:
-  with open('{0}_{1}_{2}.pdf'.format(market_type,name,str(weekNumber)), 'wb') as pdf:
-    pdf.write(myfile.content)
+for city in city_codes: #for loop to loop through the location keys
+ url = "https://static.edeka.de/media/handzettel/MINDEN/"+city+"/blaetterkatalog/pdf/complete.pdf" #API URL
+ myfile = requests.get(url) #gettting the info from the API using the get method of the requests library
+ for name in market_name: #for loop to loop through the nomenclature variables
+  with open('{0}_{1}_{2}.pdf'.format(market_type,name,str(weekNumber)), 'wb') as pdf: #opening file to save the PDF
+    pdf.write(myfile.content) #writing of the PDF
 
--- Marktkauf.py
+++ Marktkauf.py
@@ -1,21 +1,22 @@
-import requests
-from datetime import date
+#!/usr/bin/env python3
+import requests #imports the requests library
+from datetime import date #imports the date class from datetime lbrary
 
-market_type = "Marktkauf"
+market_type = "Marktkauf" #var to set the market type
 
 market_name = ["Loehne", "Herford", "Buende", "Espelkamp", "Belm", "Rinteln", "Bad_Salzuflen" ,"Hameln", "Osnabrueck", "Luebbecke", "Meppen",
-"Wilhelmshaven", "Osterholz_Scharmbeck", "Wunstorf", "Hannover"]
+"Wilhelmshaven", "Osterholz_Scharmbeck", "Wunstorf", "Hannover"] # array of market names
 
-weekNumber = date.today().isocalendar()[1]
+weekNumber = date.today().isocalendar()[1] #var which stores the ISO week number
 
 url_parts = ["20018_Loehne", "20004_Herford", "20009_Buende", "20071_Espelkamp", "20015_Belm", "20014_Rinteln", "20013_Bad_Salzuflen", 
 "20046_Hameln", "20002_Osnabrueck", "20007_Luebbecke", "20017_Meppen", "20026_Wilhelmshaven", "20102_Osterholz_Scharmbeck", "20022_Wunstorf", 
 "20025_Hannover"]
 
-for part in url_parts:
- url = "https://static.edeka.de/media/handzettel/MINDEN/MARKTKAUF_"+part+"/blaetterkatalog/pdf/complete.pdf"
- myfile = requests.get(url)
- for name in market_name:
-  with open('{0}_{1}_{2}.pdf'.format(market_type,name,str(weekNumber)), 'wb') as pdf:
-    pdf.write(myfile.content)
+for part in url_parts: #for loop to loop through the location keys
+ url = "https://static.edeka.de/media/handzettel/MINDEN/MARKTKAUF_"+part+"/blaetterkatalog/pdf/complete.pdf" #API URL
+ myfile = requests.get(url) #gettting the info from the API using the get method of the requests library
+ for name in market_name: #for loop to loop through the nomenclature variables
+  with open('{0}_{1}_{2}.pdf'.format(market_type,name,str(weekNumber)), 'wb') as pdf: #opening file to save the PDF
+    pdf.write(myfile.content) #writing of the PDF
 
